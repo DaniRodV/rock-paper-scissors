b492a6a1d0292343938bf8fb71441ae9df8c683d
Daniel R <174905798+DaniRodV@users.noreply.github.com>
2024-08-11 11:48:33 -0400

    Create function to determine computer choice

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function getComputerChoice() {
+    // Generate random number between 1 and 3
+   const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+   // Determine computer's choice based on the random number
+   if (randomNumber === 1) {
+    return "rock";
+   } else if (randomNumber === 2) {
+    return "paper";
+   } else {
+    return "scissors";
+   }
+}
+
+const computerChoice = getComputerChoice();
+console.log(`Computer chose: ${computerChoice}`);
