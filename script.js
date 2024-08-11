41fcab782a29470f2b67e05ff806935bbb37af42
Daniel R <174905798+DaniRodV@users.noreply.github.com>
2024-08-11 14:37:38 -0400

    Clean up code for better readability

--- script.js
+++ script.js
@@ -1,66 +1,61 @@
-// Score variables
 let humanScore = 0;
 let computerScore = 0;
 
-function displayScores() {
-    console.log(`Player score: ${humanScore}`);
+function displayScores () {
+    console.log(`Your score: ${humanScore}`);
     console.log(`Computer score: ${computerScore}`);
 }
 
 displayScores();
 
 function playRound (humanChoice, computerChoice) {
-    
     let roundResult;
 
     if (humanChoice === computerChoice) {
-        roundResult = "It's a tie.";
+        roundResult = `It's a tie.`;
     } else if (
         (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "paper" && computerChoice === "rock") ||
-    (humanChoice === "scissors" && computerChoice === "paper")) {
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")) {
         roundResult = `You win! ${humanChoice} beats ${computerChoice}`;
         humanScore++;
     } else {
-        roundResult = `You lose! ${computerChoice} beats ${humanChoice}`;
+        roundResult = `You lose! ${computerChoice} beats ${computerChoice}`;
         computerScore++;
     }
 
     console.log(roundResult);
     return roundResult;
+
 }
 
-function getComputerChoice() {
-    // Generate random number between 1 and 3
-   const randomNumber = Math.floor(Math.random() * 3) + 1;
+function getComputerChoice () {
+    const randomNumber = Math.floor(Math.random()* 3) + 1;
 
-   // Determine computer's choice based on the random number
-   if (randomNumber === 1) {
-    return "rock";
-   } else if (randomNumber === 2) {
-    return "paper";
-   } else {
-    return "scissors";
-   }
+    if (randomNumber === 1) {
+        return "rock";
+    } else if (randomNumber === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
 function getHumanChoice () {
-    //Get human choice
-    let playerInput = prompt("Rock, paper, scissors?", "").toLowerCase();
+    const humanInput = prompt("Rock, paper or scissors?", "").toLowerCase();
 
-    if (playerInput === "rock" || playerInput === "paper" || playerInput === "scissors") {
-        return playerInput;
+    if (humanInput === "rock" || humanInput === "paper" || humanInput === "scissors") {
+        return humanInput;
     } else {
-        alert("Invalid input. Try again.");
-        return "Invalid";
+        alert("Invalid input. Try again with one of the three options given.");
+        return "Invalid.";
     }
 }
 
 const humanSelection = getHumanChoice();
-console.log(`Player chose: ${humanSelection}`);
-
+console.log(humanSelection);
 const computerSelection = getComputerChoice();
-console.log(`Computer chose: ${computerSelection}`);
+console.log(computerSelection);
 
 playRound(humanSelection, computerSelection);
 
